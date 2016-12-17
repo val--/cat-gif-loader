22bc5e2c998e8b6dd99c004f5154df4a7896bf03
Valentin Guillaume <khayvav@gmail.com>
2016-12-17 16:09:35 +0000

    added a 'share to Facebook' button

--- src/components/catgif.js
+++ src/components/catgif.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {ShareButtons,} from 'react-share';
+const {FacebookShareButton} = ShareButtons;
 
 class CatGif extends React.Component {
     
@@ -6,7 +8,7 @@ class CatGif extends React.Component {
     super();
         this.state = {
           catsSeen: 0,
-          gifLoaded: true
+          currentCatSrc:''
         };
     }
     
@@ -14,35 +16,85 @@ class CatGif extends React.Component {
         
         var theCat = document.getElementById("theCat");
         var logo = document.getElementById("logo");
-        logo.style.webkitAnimationPlayState = "paused";
+        var button = document.getElementById("buttonLoadCat");
+        var facebookButton = document.getElementById("buttonFacebookShare");
         
-        theCat.src = "http://thecatapi.com/api/images/get?format=src&type=gif" + "&" + "rand=" + Math.random(0,99999);
+        logo.style.webkitAnimationPlayState = "paused";
+        button.textContent = "Loading new cat...";
+        button.classList.add("disabled");
+        facebookButton.classList.add("disabled");
+        
+        var image_url = this.httpGet('http://thecatapi.com/api/images/get.php?format=xml&type=gif');
+        
+        theCat.src = image_url;
+        this.setState({ currentCatSrc:image_url})
+        
+    }
+    
+    httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+
+        var xmlDoc = xmlHttp.responseXML;
+        var url = "";
+        var x = xmlDoc.getElementsByTagName("url");
+        for (var i = 0; i < x.length;  i++) {
+            url += x[i].childNodes[0].nodeValue;
+        }
+        return url;
     }
     
     handleImageLoaded(){
         this.setState({ catsSeen:++this.state.catsSeen})
+        var button = document.getElementById("buttonLoadCat");
+        var facebookButton = document.getElementById("buttonFacebookShare");
         var logo = document.getElementById("logo");
+        
         logo.style.webkitAnimationPlayState = "running";
+        
+        button.classList.remove("disabled");
+        facebookButton.classList.remove("disabled");
+        
+        button.textContent = "Load a new cat ! ";
     }
     
     handleImageErrored(){
         this.loadCat();
     }
     
+    
     render() {
+        
     return(
         <div className="container-fluid" id="gifContainer">
             <div className="imageContainer">
                 <img 
                 id="theCat" 
-                src="http://thecatapi.com/api/images/get?format=src&type=gif" 
+                src="" 
                 onLoad={ () => this.handleImageLoaded()}
                 onError={ () => this.handleImageErrored()}
                 />
             </div>
             <div className="buttonsContainer">
-                <button className="myButton" onClick={() => this.loadCat()}>Load a new cat</button>
+            
+                <button 
+                    className="button myButton" 
+                    id="buttonLoadCat" 
+                    onClick={() => this.loadCat()}>
+                    Load a new cat ! 
+                </button>
+                
+                <FacebookShareButton
+                    url={this.state.currentCatSrc}
+                    title='Meow !'>
+                    <button className="button facebookButton" id="buttonFacebookShare" onClick={() => this.shareOnFacebook()}>Share on Facebook </button>
+                </FacebookShareButton>
+                
                 <p className="lead">You have seen <span className="catsSeen">{this.state.catsSeen}</span> cat gif. <br/><br/>Don't you have anything better to do ? 🐱‍👓</p>
+
             </div>
         </div>
         );
